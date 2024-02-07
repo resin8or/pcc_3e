91a55d66381dc579151060234264887ab053cd19
resin8or <jamie.tobin@gmail.com>
2024-02-07 14:03:19 +0000

    Including changes for functions in CH8
    Function code as detailed in CH8

---
+++ chapter_08/making_more_pizzas.py
@@ -0,0 +1,5 @@
+from pizza import make_pizza as mp
+# could have used * in the import statement to import all functions from the module
+
+mp(16, "pepperoni")
+mp(12, "mushrooms", "green peppers", "extra cheese")
